67e9bfe9e547c114ee86fb660f7d26031e90aad0
Susi <susmitadumirai@outlook.com>
2017-11-20 22:18:55 +0000

    Updated git ignore, added maven project.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Susi on 20/11/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Susi on 20/11/2017.
+ */
+public class MainTest {
+
+    @Test
+    public void testingMain() {
+        assertThat(true, is(true));
+    }
+
+}
