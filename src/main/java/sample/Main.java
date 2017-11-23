d4311bbe4d52016dc35f97a29ad1dc10245f37f9
Susmita Rai <09srai4@gmail.com>
2017-11-23 12:05:16 +0000

    Created FX project with SQLLite maven dependency

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+    //    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        BorderPane root = new BorderPane();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
