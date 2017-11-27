e3f8163b7b656e7fc7968c8bd7723e1f796ef301
Leni <pceneva@gmail.com>
2017-11-27 15:46:58 +0000

    created basic login gui incomplete

--- src/main/java/GUI/Login.java
+++ src/main/java/GUI/Login.java
@@ -3,16 +3,32 @@ package GUI;/**
  */
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Login extends Application {
+
+    private final int GAP  = 10;
+    private final int INSETS  = 25;
+    private final int SCENE_WIDTH = 400;
+    private final int SCENE_HEIGHT = 300;
+
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
     public void start(Stage primaryStage) {
 
+        BorderPane pane = new BorderPane();
+
+        Scene scene = new Scene(pane, SCENE_WIDTH, SCENE_HEIGHT);
+        primaryStage.setScene(scene);
+
+        primaryStage.setTitle("Artatawe");
+
+        primaryStage.show();
     }
 }
