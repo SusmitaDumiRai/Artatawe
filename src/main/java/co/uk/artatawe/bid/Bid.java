37984aa46815a4bb06a73123ffd3d1f9c3922ebb
Finz95 <33931822+Finz95@users.noreply.github.com>
2017-11-28 20:44:30 +0000

    bid class

--- src/main/java/co/uk/artatawe/bid/Bid.java
+++ src/main/java/co/uk/artatawe/bid/Bid.java
@@ -1,4 +1,58 @@
 package co.uk.artatawe.bid;
 
+/**
+ * creates bid object
+ * utilizes bidFileManager to store bid obj in txt file
+ */
+
 public class Bid {
+
+    //should be type User but causes an error as there's no User class yet
+    private String buyer;
+    private double bidAmount;
+    private String dateAndTime;
+
+
+    /**
+     * constructor for bid
+     * @param buyer
+     * @param bidAmount
+     * @param dateAndTime
+     */
+    public Bid(String buyer, double bidAmount, String dateAndTime) {
+        this.buyer = buyer;
+        this.bidAmount = bidAmount;
+        this.dateAndTime = dateAndTime;
+
+    }
+
+    /**
+     * gets the user that placed the bid
+     * @return buyer
+     */
+    public String getBuyer() {
+        return buyer;
+    }
+
+    /**
+     * gets the amount that the user bid on the auction
+     * @return bidAmount
+     */
+    public double getBidAmount() {
+        return bidAmount;
+    }
+
+    /**
+     * gets the date and time that the bid was placed
+     * @return dateAndTime
+     */
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    //add method to give bid obj to bidFileManager
+
+
+
 }
+
