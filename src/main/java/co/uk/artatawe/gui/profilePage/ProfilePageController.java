c09c251fd2fe7f5df5ff225ed8e20f773a8055af
Kraken Leni <pceneva@gmail.com>
2017-12-02 13:32:25 +0000

    some gui samples

---
+++ src/main/java/co/uk/artatawe/gui/profilePage/ProfilePageController.java
@@ -0,0 +1,12 @@
+package co.uk.artatawe.gui.profilePage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class ProfilePageController implements Initializable{
+
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
