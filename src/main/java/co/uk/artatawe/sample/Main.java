f0fcf4da72de6ac9a785b3f1eca8b3d4ba3e059f
Susi <susmitadumirai@outlook.com>
2017-12-02 19:44:35 +0000

    Added more examples of artwork sql

--- src/main/java/co/uk/artatawe/sample/Main.java
+++ src/main/java/co/uk/artatawe/sample/Main.java
@@ -4,6 +4,8 @@ import co.uk.artatawe.database.ArtworkDatabaseManager;
 import co.uk.artatawe.database.AuctionDatabaseManager;
 import co.uk.artatawe.database.BidDatabaseManager;
 import co.uk.artatawe.database.UserDatabaseManager;
+
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -17,19 +19,25 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         //Parent root = FXMLLoader.load(getClass().getResource("co.uk.artatawe.sample.fxml"));
         BorderPane root = new BorderPane();
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+
+
+
 
         primaryStage.show();
     }
 
-/*
+
     public static void main(String[] args) {
         launch(args);
 
     }
-    */
 
+
+
+/*
     public static void main(String[] args) {
         UserDatabaseManager userDatabaseManager = new UserDatabaseManager();
         ArtworkDatabaseManager artworkDatabaseManager = new ArtworkDatabaseManager();
@@ -37,7 +45,6 @@ public class Main extends Application {
         BidDatabaseManager bidDatabaseManager = new BidDatabaseManager();
 
 
-
     }
+*/
 }
-;
