75096cc701fb3298ee3309289ff4630df48e52ff
Kraken Leni <pceneva@gmail.com>
2017-12-03 13:28:07 +0000

    fixed merge conflict

--- src/main/java/co/uk/artatawe/sample/Main.java
+++ src/main/java/co/uk/artatawe/sample/Main.java
@@ -22,11 +22,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+       /* String imagePath = "co/uk/artatawe/artworkpictures/Giraffe.png";
+        ImageView imageView = new ImageView(imagePath);*/
+
 
 
         // Display image on screen
         StackPane root = new StackPane();
-        
+
+       // root.getChildren().add(imageView);
+
+
         Scene scene = new Scene(root, 600, 500);
 
         primaryStage.setTitle("Image Read Test");
@@ -41,15 +47,21 @@ public class Main extends Application {
 
 
 
-    public static void main(String[] args) {
-        launch(args);
 
-    }
+//    public static void main(String[] args) {
+//        launch(args);
+//
+//
+//    }
 
 
 
 
-    /*
+
+
+
+
+
     public static void main(String[] args) {
         UserDatabaseManager userDatabaseManager = new UserDatabaseManager();
         ArtworkDatabaseManager artworkDatabaseManager = new ArtworkDatabaseManager();
@@ -57,6 +69,7 @@ public class Main extends Application {
         BidDatabaseManager bidDatabaseManager = new BidDatabaseManager();
 
         userDatabaseManager.getAllUsers();
+
 
         String sqlInsertArtwork = "INSERT INTO artwork (title, description, photo, nameofcreator, reservedprice," +
                 "dateentered, bidsallowed, typeofartwork, width, height) values ('Bar reflection', 'This piece is themed on two young people lost in each other, sitting in a mirrored booth in a public house - in the style he is now well known for. The back of the painting is signed by the artist, and both young people, Carra and Luke who sat for the creation of this work.'," +
@@ -66,6 +79,7 @@ public class Main extends Application {
         artworkDatabaseManager.executeStatement(sqlInsertArtwork);
 
 
+
     }
-*/
+
 }
