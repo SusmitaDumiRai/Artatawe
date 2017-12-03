aad29267f77150717d0ddff7201099a38559f776
Susi <susmitadumirai@outlook.com>
2017-12-03 18:19:49 +0000

    Authenticates user, does not open new window

---
+++ src/main/java/co/uk/artatawe/controller/BrowseAuctionController.java
@@ -0,0 +1,12 @@
+package co.uk.artatawe.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * Created by Susi on 03/12/2017.
+ */
+public class BrowseAuctionController {
+
+
+}
---
+++ src/main/java/co/uk/artatawe/controller/ProfilePageController.java
@@ -0,0 +1,29 @@
+package co.uk.artatawe.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * Created by Susi on 03/12/2017.
+ */
+public class ProfilePageController {
+
+    /*
+
+               </HBox>
+                  <Label fx:id="telephoneNumber" prefHeight="300.0" prefWidth="300.0" />
+                  <Label fx:id="address" prefHeight="300.0" prefWidth="300.0" />
+     */
+    @FXML
+    private Label firstName;
+
+    @FXML
+    private Label lastName;
+
+
+
+
+
+
+
+}
