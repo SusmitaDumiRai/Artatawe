1be253a08b2bddcccf4e08f1f9283696dedb5850
Susi <susmitadumirai@outlook.com>
2017-12-04 20:18:51 +0000

    Added validation for create auction

--- src/main/java/co/uk/artatawe/controller/CreateAuctionController.java
+++ src/main/java/co/uk/artatawe/controller/CreateAuctionController.java
@@ -1,5 +1,6 @@
 package co.uk.artatawe.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -17,25 +18,7 @@ public class CreateAuctionController  implements Initializable {
     private Label sellerName;
 
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    private Button sumbitButton;
-
-    @FXML
-    private Button myActions;
-
-    @FXML
-    private Button profile;
-
-    @FXML
-    private Button artwork;
-
-    @FXML
-    private Button bidHistory;
-
-    @FXML
-    private ToggleGroup typeArtwork;
+    private Button submitButton;
 
     @FXML
     private RadioButton paintingRadioButton;
@@ -44,41 +27,41 @@ public class CreateAuctionController  implements Initializable {
     private RadioButton sculptureRadioButton;
 
     @FXML
-    private Button users;
+    private TextField title; //artwork title.
 
     @FXML
-    private Button auction;
+    private TextField creatorName;
+
+    @FXML
+    private TextField year; //year created by creator.
 
 
     @FXML
     private TextField width;
 
     @FXML
-    private TextField allowedBids;
+    private TextField height;
+
+    @FXML
+    private TextField depth;
 
     @FXML
     private TextField reservedPrice;
 
     @FXML
-    private TextField title;
-
-    @FXML
-    private TextField height;
-
-    @FXML
-    private TextField creatorName;
-
-    @FXML
-    private TextField year;
+    private TextField allowedBids;
 
     @FXML
     private TextArea description;
 
+    @FXML
+    private TextField material;
 
 
-
-
-
+    /**
+     * Display current seller's name.
+     * @param username username of logged in user.
+     */
     public void changeSellerUsername(String username) {
         sellerName.setText(username);
     }
@@ -86,6 +69,132 @@ public class CreateAuctionController  implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML
+    void handleSubmitAction(ActionEvent event) {
+        valWidth();
+        valDepth();
+        valHeight();
+    }
+
+    public void createAuction() {
+
+    }
+
+    /**
+     * Validates user entered value for title.
+     * @return true if not empty.
+     */
+    public boolean valTitle() {
+        return !(title.getText().isEmpty()); //return false is empty.
+    }
+
+
+    /**
+     * Validates user entered value for creator name.
+     * @return true if not empty.
+     */
+    public boolean valCreatorName() {
+        return !(creatorName.getText().isEmpty()); //return false is empty.
+    }
+
+    /**
+     * Validates user entered value for year.
+     * @return true if int.
+     */
+    public boolean valYear() {
+        try {
+            Integer.parseInt(year.getText());
+            return true;
+        } catch (NumberFormatException ex) {
+            System.out.println("enter a number."); //needs to be changed.
+            //TODO nice error message.
+        }
+        return false;
+    }
+
+
+    /**
+     * Validates user entered double value for width.
+     * @return true if double.
+     */
+    public boolean valWidth() {
+        try {
+            Float.parseFloat(width.getText());
+            return true;
+        } catch (NumberFormatException ex) {
+            System.out.println("enter digits only plz"); // needs to be cahnged.
+            //TODO display error message
+        }
+        return false;
+    }
+
+    /**
+     * Validates user entered double value for height.
+     * @return true if double.
+     */
+    public boolean valHeight() {
+        try {
+            Float.parseFloat(height.getText());
+            return true;
+        } catch (NumberFormatException ex) {
+            System.out.println("enter digits only plz"); // needs to be cahnged.
+            //TODO display error message
+        }
+        return  false;
+    }
+
+    /**
+     * Validates user entered double value for depth.
+     * @return true if double.
+     * */
+    public boolean valDepth() {
+        try {
+            Float.parseFloat(depth.getText());
+            return true;
+        } catch (NumberFormatException ex) {
+            System.out.println("enter digits only plz"); // needs to be cahnged.
+            //TODO display error message
+        }
+        return false;
+    }
+
+    /**
+     * Validates user entered double value for reserved price.
+     * @return true if double.
+     */
+    public boolean valReservedPrice() {
+        try {
+            Float.parseFloat(reservedPrice.getText());
+            return true;
+        } catch (NumberFormatException ex) {
+            System.out.println("enter digits only plz"); // needs to be cahnged.
+            //TODO display error message
+        }
+        return false;
+    }
+
+    /**
+     * Validates user entered value for year.
+     * @return true if int.
+     */
+    public boolean valAllowedBids() {
+        try {
+            Integer.parseInt(allowedBids.getText());
+            if ( Integer.parseInt(allowedBids.getText()) > 0) {
+                return true;
+            } else {
+                System.out.println("enter a number more than 0 plz.");
+                return false;
+                //TODO nice error message.
+            }
+
+        } catch (NumberFormatException ex) {
+            System.out.println("enter a number."); //needs to be changed.
+            //TODO nice error message.
+        }
+        return false;
     }
 
 
