e8b456c8bd508b9f61128861e442b55fdbff5d8c
Susi <susmitadumirai@outlook.com>
2017-12-04 19:33:37 +0000

    Added all variables for create auction.

--- src/main/java/co/uk/artatawe/controller/CreateAuctionController.java
+++ src/main/java/co/uk/artatawe/controller/CreateAuctionController.java
@@ -2,7 +2,7 @@ package co.uk.artatawe.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,9 +13,69 @@ import java.util.ResourceBundle;
  */
 public class CreateAuctionController  implements Initializable {
 
-
     @FXML
     private Label sellerName;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private Button sumbitButton;
+
+    @FXML
+    private Button myActions;
+
+    @FXML
+    private Button profile;
+
+    @FXML
+    private Button artwork;
+
+    @FXML
+    private Button bidHistory;
+
+    @FXML
+    private ToggleGroup typeArtwork;
+
+    @FXML
+    private RadioButton paintingRadioButton;
+
+    @FXML
+    private RadioButton sculptureRadioButton;
+
+    @FXML
+    private Button users;
+
+    @FXML
+    private Button auction;
+
+
+    @FXML
+    private TextField width;
+
+    @FXML
+    private TextField allowedBids;
+
+    @FXML
+    private TextField reservedPrice;
+
+    @FXML
+    private TextField title;
+
+    @FXML
+    private TextField height;
+
+    @FXML
+    private TextField creatorName;
+
+    @FXML
+    private TextField year;
+
+    @FXML
+    private TextArea description;
+
+
+
 
 
 
@@ -27,4 +87,6 @@ public class CreateAuctionController  implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+
 }
