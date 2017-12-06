2a9ceb2adef947696e9c38f29b12d21d895b3527
Finz95 <33931822+Finz95@users.noreply.github.com>
2017-12-06 18:29:57 +0000

    Start of bid history gui

---
+++ src/main/java/co/uk/artatawe/controller/BidHistoryController.java
@@ -0,0 +1,46 @@
+package co.uk.artatawe.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+/**
+ * Controller for bid history page.
+ */
+public class BidHistoryController {
+
+    @FXML
+    private Button auctionButton;
+
+    @FXML
+    private Button bidHistoryButton;
+
+    @FXML
+    private Button Button;
+
+    @FXML
+    private Button buyingHistoryButton;
+
+    @FXML
+    private Button sellingHistoryButton;
+
+    @FXML
+    private ListView<?> wonAuctionsList;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private Button myActionsButton;
+
+    @FXML
+    private ListView<?> placedBidsList;
+
+    @FXML
+    private Button profileButton;
+
+    @FXML
+    private Button favoritesButton;
+
+}
