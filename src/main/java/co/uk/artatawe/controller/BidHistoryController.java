ef3196b25c7187fcc351caaddccae88485b0d6d4
Finz95 <33931822+Finz95@users.noreply.github.com>
2017-12-07 12:42:50 +0000

    additions to bid history controller

--- src/main/java/co/uk/artatawe/controller/BidHistoryController.java
+++ src/main/java/co/uk/artatawe/controller/BidHistoryController.java
@@ -1,14 +1,40 @@
 package co.uk.artatawe.controller;
 
+import co.uk.artatawe.database.AuctionDatabaseManager;
+import co.uk.artatawe.database.BidDatabaseManager;
+import co.uk.artatawe.main.Auction;
+import co.uk.artatawe.main.Bid;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+
 /**
  * Controller for bid history page.
+ * @author James Finlayson 905234
  */
-public class BidHistoryController {
+public class BidHistoryController implements Initializable {
+
+    private String username; //logged in user.
+    private final int WIDTH = 800; //size of window.
+    private final int HEIGHT = 600; //size of window.
+
+    /**
+     * Sets username.
+     * @param username username of logged in user.
+     */
+    public BidHistoryController(String username) {
+        this.username = username;
+    }
 
     @FXML
     private Button auctionButton;
@@ -23,10 +49,15 @@ public class BidHistoryController {
     private Button buyingHistoryButton;
 
     @FXML
+    private Button usersButton;
+
+    @FXML
     private Button sellingHistoryButton;
 
     @FXML
-    private ListView<?> wonAuctionsList;
+    private ListView<Auction> wonAuctionsList;
+
+    ObservableList<Auction> wonAuctions = FXCollections.observableArrayList(getWonAuctions());
 
     @FXML
     private Label welcomeText;
@@ -35,12 +66,81 @@ public class BidHistoryController {
     private Button myActionsButton;
 
     @FXML
-    private ListView<?> placedBidsList;
+    private ListView<Bid> placedBidsList;
+
+    ObservableList<Bid> placedBids = FXCollections.observableArrayList(getPlacedBids());
+
+    @FXML
+    private ListView<Auction> soldAuctionsList;
+
+    ObservableList<Auction> soldAuctions = FXCollections.observableArrayList(getSoldAuctions());
 
     @FXML
     private Button profileButton;
 
-    @FXML
-    private Button favoritesButton;
+    /**
+     * Gets list of won auctions for logged in user.
+     */
+    public ArrayList getWonAuctions(){
 
+        AuctionDatabaseManager auctionDatabaseManager = new AuctionDatabaseManager();
+
+        String sqlSelect = "Select * from auction where auctionComp = 1 and winningBid = '" + this.username + "';";
+
+        ArrayList<Auction> wonAuctionArrayList = auctionDatabaseManager.getAllAuctions(sqlSelect);
+
+        return wonAuctionArrayList;
+
+    }
+
+    /**
+     * Gets list of placed bids for logged in user.
+     */
+    public ArrayList getPlacedBids(){
+
+        BidDatabaseManager bidDatabaseManager = new BidDatabaseManager();
+
+        String sqlSelect = "Select * from bid where buyer = '" + this.username + "';";
+
+        ArrayList<Bid> placedBidsArrayList = bidDatabaseManager.getAllBids(sqlSelect);
+
+        return placedBidsArrayList;
+
+    }
+
+    /**
+     * Gets list of sold auctions for logged in user.
+     */
+    public ArrayList getSoldAuctions(){
+
+        AuctionDatabaseManager auctionDatabaseManager = new AuctionDatabaseManager();
+
+        String sqlSelect = "Select * from auction where auctioncomp = true and seller = '" + this.username + "';";
+
+        ArrayList<Auction> soldAuctionsArrayList = auctionDatabaseManager.getAllAuctions(sqlSelect);
+
+        return soldAuctionsArrayList;
+
+    }
+
+    @FXML
+    void handleBuyingHistoryButton(ActionEvent event){
+        placedBidsList.setVisible(true);
+        wonAuctionsList.setVisible(true);
+
+    }
+
+    @FXML
+    void handleSellingHistoryButton(ActionEvent event){
+        soldAuctionsList.setVisible(true);
+
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        placedBidsList.setVisible(false);
+        wonAuctionsList.setVisible(false);
+        soldAuctionsList.setVisible(false);
+    }
 }
